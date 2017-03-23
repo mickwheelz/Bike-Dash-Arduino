158bdd8ce96c7aa6a15ba643381ae1cd7e850eb2
Mick Wheeler <mick.wheelz@gmail.com>
2017-03-23 14:49:24 +0000

    major refactor
    Now using JSON output, has working power management, generates random
    RPM for testing and uses VR for temperature testing

--- Bike-Dash-Arduino.ino
+++ Bike-Dash-Arduino.ino
@@ -1,7 +1,10 @@
 #include <TinyGPS++.h>
 #include <SoftwareSerial.h>
+#include <ArduinoJson.h>
 
-static const int RXPin = 3, TXPin = 2; //TX and RX pins for GPS
+//TX and RX pins for GPS
+static const int RXPin = 3, TXPin = 2; 
+//GPS Speed
 static const uint32_t GPSBaud = 9600;
 
 int pinPowerState = 6;
@@ -13,37 +16,39 @@ int pinTEMP = A1;
 
 int currentMode = 1;
 
-
-// The TinyGPS++ object
 TinyGPSPlus gps;
-SoftwareSerial ss(RXPin, TXPin); //SS for GPS
+///SS for GPS
+SoftwareSerial ss(RXPin, TXPin);
 
 void setup() {
   Serial.begin(115200);
   ss.begin(GPSBaud);
 
-  Serial.println("Format");
-  Serial.println("RPM|SPEED|LAT|LNG|TEMP|MODE|TIME");
-
   pinMode(pinRPI, OUTPUT);
   pinMode(pinMODE, INPUT);
   pinMode(pinRPM, INPUT);
   pinMode(pinTEMP, INPUT);
+  pinMode(pinPowerState, INPUT);
 }
 
 void loop() {
-  //           RPM         |    MPH                |    LATITUDE              |    LONGETUDE             |    TEMPERATUE    |    MODE         |    TIME
-  Serial.print(getRPM() + "|" + gps.speed.mph() + "|" + gps.location.lat() + "|" + gps.location.lng() + "|" + getTemp() + "|" + setMode() + "|" + gps.time.value() ) ;
-  Serial.println(); //NEW LINE
   
-  smartDelay(500);
-
-  if (millis() > 5000 && gps.charsProcessed() < 10) {
-     //   Serial.println(F("No GPS data received: check wiring"));
-  }
-  
-  doRaspberryPiPowerMgmt();
-
+    if(isBikeOn()) {
+        JsonObject& root = buildJSON();
+        root.printTo(Serial);
+        Serial.println();
+        delay(500);
+    }
+    else {
+      //output once to shutdown RPI
+      JsonObject& root = buildJSON();
+      root.printTo(Serial); 
+      Serial.println();
+      delay(500);
+      while(!isBikeOn()) {
+        //TODO: Sleep mode
+      }
+    }
 }
 
 // This custom version of delay() ensures that the gps object is always being fed
@@ -59,7 +64,7 @@ String getRPM() {
   int rpmState = analogRead(pinRPM);
   //TODO: some math to get correct value
   //return rpmState;
-  return "00000";
+  return String(random(1000, 13000));
 }
 
 String getTemp() {
@@ -69,30 +74,50 @@ String getTemp() {
   return String(tempState);
 }
 
-String setMode() {
-    if(digitalRead(pinMode) == HIGH) {
-      if(currentMode < 4) {
-          currentMode++;
-          delay(1000); //wait a second before accepting another press
-      }
-      else {
-        currentMode = 1;
-        delay(1000); //wait a second before accepting another press
-      }
-    }
-    return String(currentMode);
-}
-
-void doRaspberryPiPowerMgmt() {
-  bool hasGoneHigh = false; //check if we have pulsed the pin
-    if(!hasGoneHigh && digitalRead(pinPowerState) == HIGH) { //if we havent and thw powerState pin is high (meaning bike is on) pulse the pin to boot the rpi
-      digitalWrite(pinRPI, HIGH);
-      hasGoneHigh = true;
-    }
-  if(hasGoneHigh && digitalRead(pinPowerState) == LOW) { //if we have pulsed it, and we lose power to the powerState pin, pulse again to init shutdown of rpi
-    delay(60000); //wait a minute after bike power off
+boolean isBikeOn() {
+  //pulses pin to wake up RPI when bike first turns on
+  boolean pinPulse = false;
+  if(digitalRead(pinPowerState)) {
     digitalWrite(pinRPI, HIGH);
+    delay(100);
+    digitalWrite(pinRPI, LOW);
+    pinPulse = true;
   }
+  else {
+    pinPulse = false;
+  }
+  return (digitalRead(pinPowerState) == HIGH);
 }
 
+JsonObject& buildJSON() {
 
+  StaticJsonBuffer<200> jsonBuffer;
+  JsonObject& root = jsonBuffer.createObject();
+  
+  root["status"] = "OK";
+  root["mode"] = "1";//setMode();
+  root["time"] = gps.time.value();
+
+  JsonObject& gpsData = root.createNestedObject("gpsData");
+  
+  gpsData["Status"] = "OFFLINE";
+  
+  if(gps.sentencesWithFix() == 0) {
+    gpsData["Status"] = "ONLINE - NO FIX";
+  }
+  else {
+     gpsData["Status"] = "ONLINE";
+  }
+  
+  gpsData["LAT"] = gps.location.lat(); 
+  gpsData["LNG"] = gps.location.lng();
+  gpsData["MPH"] = gps.speed.mph();
+  gpsData["KPH"] = gps.speed.kmph();
+
+  JsonObject& bikeData = root.createNestedObject("bikeData");
+  bikeData["RPM"] = getRPM(); 
+  bikeData["Temp"] = getTemp();
+  bikeData["Power"] = isBikeOn();
+
+  return root;
+}
